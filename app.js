4b38a79828fbeddd7eb74ad9597d47570b147c7e
John Doe <myehia162@gmail.com>
2023-10-26 22:23:36 +0300

    eslinter & prettier configration

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const { rateLimit } = require('express-rate-limit');
 const helmet = require('helmet');
 const path = require('path');
-const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const globalErrorHandler = require('./controllers/errorController');
 
-//express app
 const app = express();
 
 // Development logging
-if (process.env.NODE_ENV == 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
 }
 
 // Data sanitization against XSS => prevent XSS attacks
@@ -24,42 +24,38 @@ app.use(xss());
 app.use(helmet());
 
 // Body parser, reading data from body into req.body
-const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
-
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
 
 // compress the responses
 app.use(compression());
 
 // limit the traffic => prevent DoS attacks
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 60 minutes
-  limit: 300, // Limit each IP to 300 requests per `window` (here, per 60 minutes)
-  message: {
-    message: 'Too many requests, try again in an hour',
-    status: 'warning',
-  },
+    windowMs: 60 * 60 * 1000, // 60 minutes
+    limit: 300, // Limit each IP to 300 requests per `window` (here, per 60 minutes)
+    message: {
+        message: 'Too many requests, try again in an hour',
+        status: 'warning',
+    },
 });
 
 // Apply the rate limiting middleware to API calls only
 app.use('/api', limiter);
 
-const cors = require('cors');
 app.use(cors({ origin: true, credentials: true }));
 
 // just a testing middelware
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  // res.locals.jwt = req.cookies.jwt;
-  // console.log(res.locals.jwt);
-  // req.session.isAuth = true;
-  // console.log(req.cookies);
-  next();
+    req.requestTime = new Date().toISOString();
+    // res.locals.jwt = req.cookies.jwt;
+    // console.log(res.locals.jwt);
+    // req.session.isAuth = true;
+    // console.log(req.cookies);
+    next();
 });
 
 // routes here
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,27 +1,23 @@
 const sendErrorProd = (err, req, res) => {};
 
-const sendErrorDev = (err, req, res) => {
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    error: err,
-    stack: err.stack,
-  });
-};
+const sendErrorDev = (err, req, res) =>
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack,
+    });
 
 module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
 
-  //production error handling
-  if (process.env.NODE_ENV.trim() == 'production') {
-    let error = Object.assign(err);
-    // error.message = err.message;
+    if (process.env.NODE_ENV.trim() === 'production') {
+        const error = Object.assign(err);
+        error.message = err.message;
 
-    sendErrorProd(error, req, res);
-
-    //development error handling
-  } else if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, req, res);
-  }
+        sendErrorProd(error, req, res);
+    } else if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, req, res);
+    }
 };
