663eb4c8908a99c96fb8c48833bab5ea9ccc8a33
MoYasser1777 <119734442+MoYasser1777@users.noreply.github.com>
2023-11-08 01:13:38 +0200

    delete/getTweets

--- routes/tweets.js
+++ routes/tweets.js
@@ -4,5 +4,7 @@ const tweetsController = require('../controllers/tweetsController');
 const router = express.Router();
 
 router.route('/add').post(tweetsController.addTweet);
+router.route('/:tweetId/deleteTweet').delete(tweetsController.deleteTweet);
+router.route('/:tweetId').get(tweetsController.getTweet);
 
 module.exports = router;
