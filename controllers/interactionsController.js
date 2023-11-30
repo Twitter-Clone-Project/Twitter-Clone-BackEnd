431fb13ec37150632fd3361bbd3a0820e7054f4c
daniel <daniel.atallah01@gmail.com>
2023-11-30 17:57:58 +0200

    Block and Muted apis

--- controllers/interactionsController.js
+++ controllers/interactionsController.js
@@ -3,6 +3,8 @@ const catchAsync = require('../middlewares/catchAsync');
 const AppError = require('../services/AppError');
 
 const Follow = require('../models/relations/Follow');
+const Mute = require('../models/relations/Mute');
+const Block = require('../models/relations/Block');
 const User = require('../models/entites/User');
 
 const filterObj = (result) => {
@@ -215,5 +217,165 @@ exports.unFollow = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: true,
     message: 'unfollow is done successfully',
+  });
+});
+
+exports.mute = catchAsync(async (req, res, next) => {
+  const { username } = req.params;
+  const currUserId = req.currentUser.userId;
+
+  const user = await AppDataSource.getRepository(User).findOne({
+    where: {
+      username: username,
+    },
+  });
+  const mute = new Mute();
+  mute.userId = currUserId;
+  mute.mutedId = user.userId;
+  const savedMute = await AppDataSource.getRepository(Mute).save(mute);
+
+  res.status(200).json({
+    status: true,
+    message: 'mute is added successfully',
+  });
+});
+
+exports.unmute = catchAsync(async (req, res, next) => {
+  const { username } = req.params;
+  const currUserId = req.currentUser.userId;
+
+  const user = await AppDataSource.getRepository(User).findOne({
+    where: {
+      username: username,
+    },
+  });
+
+  const result = await AppDataSource.getRepository(Mute)
+    .createQueryBuilder()
+    .delete()
+    .from(Mute)
+    .where('mutedId = :mutedId AND userId = :userId', {
+      mutedId: user.userId,
+      userId: currUserId,
+    })
+    .execute();
+
+  if (!result.affected || !(result.affected > 0))
+    return next(new AppError('error in unmuting', 400));
+
+  res.status(200).json({
+    status: true,
+    message: 'unmute is done successfully',
+  });
+});
+exports.getListOfMutes = catchAsync(async (req, res, next) => {
+  // const { username } = req.params;
+  // const user = await AppDataSource.getRepository(User).findOne({
+  //   where: {
+  //     username: username,
+  //   },
+  // });
+  // const userId = user.userId;
+  const currUserId = req.currentUser.userId;
+
+  const mutesQuery = await AppDataSource.getRepository(User)
+    .createQueryBuilder('user')
+    .where('mute.userId = :userId', { userId: currUserId })
+    .innerJoin(Mute, 'mute', 'mute.mutedId = user.userId')
+    .select([
+      'user.userId',
+      'user.username',
+      'user.name',
+      'user.bio',
+      'user.imageUrl',
+    ])
+    .groupBy('user.userId')
+    .getMany();
+  let mutesList = filterObj(mutesQuery);
+  res.status(200).json({
+    status: true,
+    data: {
+      users: mutesList,
+    },
+  });
+});
+exports.block = catchAsync(async (req, res, next) => {
+  const { username } = req.params;
+  const currUserId = req.currentUser.userId;
+
+  const user = await AppDataSource.getRepository(User).findOne({
+    where: {
+      username: username,
+    },
+  });
+  const block = new Block();
+  block.userId = currUserId;
+  block.blockedId = user.userId;
+  const savedBlock = await AppDataSource.getRepository(Block).save(block);
+
+  res.status(200).json({
+    status: true,
+    message: 'block is added successfully',
+  });
+});
+
+exports.unblock = catchAsync(async (req, res, next) => {
+  const { username } = req.params;
+  const currUserId = req.currentUser.userId;
+
+  const user = await AppDataSource.getRepository(User).findOne({
+    where: {
+      username: username,
+    },
+  });
+
+  const result = await AppDataSource.getRepository(Block)
+    .createQueryBuilder()
+    .delete()
+    .from(Block)
+    .where('blockedId = :blockedId AND userId = :userId', {
+      blockedId: user.userId,
+      userId: currUserId,
+    })
+    .execute();
+
+  if (!result.affected || !(result.affected > 0))
+    return next(new AppError('error in unBlocking', 400));
+
+  res.status(200).json({
+    status: true,
+    message: 'unblock done successfully',
+  });
+});
+
+exports.getListOfBlocks = catchAsync(async (req, res, next) => {
+  // const { username } = req.params;
+  // const user = await AppDataSource.getRepository(User).findOne({
+  //   where: {
+  //     username: username,
+  //   },
+  // });
+  // const userId = user.userId;
+  const currUserId = req.currentUser.userId;
+
+  const blocksQuery = await AppDataSource.getRepository(User)
+    .createQueryBuilder('user')
+    .where('block.userId = :userId', { userId: currUserId })
+    .innerJoin(Block, 'block', 'block.blockedId = user.userId')
+    .select([
+      'user.userId',
+      'user.username',
+      'user.name',
+      'user.bio',
+      'user.imageUrl',
+    ])
+    .groupBy('user.userId')
+    .getMany();
+  let blocksList = filterObj(blocksQuery);
+  res.status(200).json({
+    status: true,
+    data: {
+      users: blocksList,
+    },
   });
 });
