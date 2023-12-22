8ad578f1194a33526f15ddb44f8d1bf041d4f61b
MoYasser1777 <119734442+MoYasser1777@users.noreply.github.com>
2023-12-22 15:46:22 +0200

    tweets unit testing

--- __tests__/routes/tweetsRouter.test.js
+++ __tests__/routes/tweetsRouter.test.js
@@ -2,12 +2,36 @@ const request = require('supertest');
 const app = require('../../app');
 const uuid = require('uuid');
 
-describe('GET /api/v1/users/:username/isUsernameFound', () => {
-  test('returns 404, username not found', async () => {
-    // const email = `testuser_${uuid.v4()}@example.com`;
-    // const res = await request(app).get('/api/v1/users/7oooda/isEmailFound');
-    // console.log(res);
-    // expect(res.statusCode).toEqual(404);
-    // expect(5).toBe(5);
+describe('POST /api/v1/tweets/add', () => {
+  jest.setTimeout(100000);
+
+  it('try to add empty tweet text and no media', async () => {
+    const response = await request(app)
+      .post('/api/v1/tweets/add')
+      .send({ tweetText: '', trends: null, media: null });
+    console.log(response.body);
+    expect(response.status).toEqual(400);
+  });
+
+  it('try to add more than 4 media in a tweet', async () => {
+    const response = await request(app)
+      .post('/api/v1/tweets/add')
+      .send({
+        tweetText: 'tweet test',
+        trends: null,
+        media: ['1.png', '2.png', '3.png', '4.png', '5.png'],
+      });
+    console.log(response.body);
+    expect(response.status).toEqual(400);
+  });
+
+  it('successfully adding non empty tweet with no media', async () => {
+    const response = await request(app).post('/api/v1/tweets/add').send({
+      tweetText: 'tweet test',
+      trends: null,
+      media: null,
+    });
+    console.log(response.body);
+    expect(response.status).toEqual(200);
   });
 });
